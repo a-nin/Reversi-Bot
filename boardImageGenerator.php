c939dbeb403264f9fec8e4fb4a117d2448be4bc6
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-06-19 23:59:34 +0900

    5.1.8

--- boardImageGenerator.php
+++ boardImageGenerator.php
@@ -8,4 +8,28 @@ define('GD_BASE_SIZE', 700);
 // 合成のベースになる画像を生成
 $destinationImage = imagecreatefrompng('imgs/reversi_board.png');
 
+// パラメーターから現在の石の配置を取得
+$stones = json_decode($_REQUEST['stones']);
+
+// 各列をループ
+for($i = 0; $i < count($stones); $i++) {
+  $row = $stones[$i];
+  // 各要素をループ
+  for($j = 0; $j < count($row); $j++) {
+    // 現在の石を生成
+    if($row[$j] == 1) {
+      $stoneImage = imagecreatefrompng('imgs/reversi_stone_white.png');
+    } else if($row[$j] == 2) {
+      $stoneImage = imagecreatefrompng('imgs/reversi_stone_black.png')
+    }
+    // 合成
+    if($row[$j] > 0) {
+      imagecopy($destinationImage, $stoneImage, 9 + (int)($j * 87.5), 9 + (int)($i * 87.5), 0, 0, 70, 70);
+
+      // 破棄
+      imagedestroy($stoneImage);
+    }
+  }
+}
+
  ?>
