6c0116c12184f3eb9fb705cb4bc76f8ca738843d
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-06-22 00:16:41 +0900

    foreach

--- index.php
+++ index.php
@@ -26,15 +26,15 @@ try {
   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
 
-// 配列に格納された各イベントをループ処理
+// 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
-  // MessageEventクラスのインスタンスでなければ処理をスキップ
+  // MessageEvent型でなければ処理をスキップ
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
     continue;
   }
-  // TextMessageクラスのインスタンスでなければ処理をスキップ
-  if(!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+  // TextMessage型でなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
     error_log('Non text message has come');
     continue;
   }
